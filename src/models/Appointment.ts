169b6a0ccf6798548fe29283b79d23099aa5d14e
paulohdelia <paulohdelia@gmail.com>
2020-11-27 21:23:40 -0300

    add typeorm

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { v4 } from 'uuid';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = v4();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
